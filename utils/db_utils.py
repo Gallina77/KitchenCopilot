d79d13a468ccfb4495c42b0e48daabafba4d100e
Gallina77 <sandra.grassl77@gmail.com>
2025-12-29 17:09:28 +0100

    Update db utils file to cache connection

--- utils/db_utils.py
+++ utils/db_utils.py
@@ -1,8 +1,11 @@
 import streamlit as st
 
-# Create the SQL connection to kitchencopilot_db as specified in your secrets file.
-conn = st.connection('data/kitchencopilot_db', type='sql')
+@st.cache_resource
+def get_connection():
+    # Create the SQL connection to kitchencopilot_db as specified in your secrets file.
+    return st.connection('kitchencopilot_db', type='sql')
 
 def find_table(table_name: str):
+    conn = get_connection()
     with conn.session as s:
         return conn.query(f"SELECT * FROM {table_name}")
