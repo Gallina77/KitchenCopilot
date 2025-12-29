73dec3bc61b9c813194f115653f1961167fa8ba4
Gallina77 <sandra.grassl77@gmail.com>
2025-12-29 16:04:33 +0100

    Delete accidentally committed files

--- utils/db_utils.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-# Create the SQL connection to kitchencopilot_db as specified in your secrets file.
-conn = st.connection('kitchencopilot_db', type='sql')
-
-def find_table(table_name: str):
-    with conn.session as s:
-        return conn.query(f"SELECT * FROM {table_name}")
