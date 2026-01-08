0ea1cf6699aa295a510bb3ee52987944d465b20b
Gallina77 <sandra.grassl77@gmail.com>
2026-01-08 07:55:51 +0100

    Fix date normalization error

--- utils/converters.py
+++ utils/converters.py
@@ -4,8 +4,6 @@ import pandas as pd
 def normalize_datetime(date):
     date = pd.to_datetime(date)
     if isinstance(date, datetime):
-        print("no change")
         return date
     else:
-        print("Change")
         return datetime.fromisoformat(date)
