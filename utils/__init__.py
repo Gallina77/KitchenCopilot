72572cf45caacc5e197f39554f5c8811843d01d5
Gallina77 <sandra.grassl77@gmail.com>
2026-01-27 07:19:40 +0100

    Refactor imports in utils/__init__.py: update db_utils import to include get_future_predictions and add render_badges from data_preparation_utils

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,8 +1,8 @@
 # utils/__init__.py
 from .weather_utils import get_weather, categorize_weather
-from .db_utils import get_holidays, save_prediction, get_todays_prediction, get_actuals_and_predictions, apply_custom_styling, calculate_metrics
+from .db_utils import get_holidays, save_prediction, get_future_predictions, get_actuals_and_predictions, apply_custom_styling, calculate_metrics
 from .prediction_utils import get_prediction
-from .data_preparation_utils import prepare_data
+from .data_preparation_utils import prepare_data, render_badges
 from .home_utils import load_model_metadata, check_database_status, check_weather_api_status, get_last_prediction_info
 from .converters import normalize_datetime  
 from .llm_insights import get_llm_insights_for_actuals_vs_predicted, get_llm_planning_insights
